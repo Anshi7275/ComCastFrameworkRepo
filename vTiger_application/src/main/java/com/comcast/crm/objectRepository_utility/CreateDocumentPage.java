5262ab737abda1df900827ecbac2ccbeacc940c1
anshi <anshi@DESKTOP-NB60TP2.mshome.net>
2024-12-13 15:54:49 +0530

    Add getter method in POM pages, CreateDocumentPage

--- vTiger_application/src/main/java/com/comcast/crm/objectRepository_utility/CreateDocumentPage.java
+++ vTiger_application/src/main/java/com/comcast/crm/objectRepository_utility/CreateDocumentPage.java
@@ -1,6 +1,8 @@
 package com.comcast.crm.objectRepository_utility;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class CreateDocumentPage {
@@ -11,6 +13,14 @@ public class CreateDocumentPage {
 		PageFactory.initElements(driver, this);  
 	}
 	
+	@FindBy(xpath = "//input[@name=\"notes_title\"]")
+	private WebElement titleTextBox;
 	
+	public WebDriver getDriver() {
+		return driver;
+	}
 
+	public WebElement getTitleTextBox() {
+		return titleTextBox;
+	}
 }
