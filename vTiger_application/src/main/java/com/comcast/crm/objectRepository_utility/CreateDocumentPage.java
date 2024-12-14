f8c25d6acf97e996973ccd79aa2d0a39714b36b3
anshi <anshi@DESKTOP-NB60TP2>
2024-12-14 10:37:38 +0530

    Update the POM page,Create document page

--- vTiger_application/src/main/java/com/comcast/crm/objectRepository_utility/CreateDocumentPage.java
+++ vTiger_application/src/main/java/com/comcast/crm/objectRepository_utility/CreateDocumentPage.java
@@ -1,6 +1,8 @@
 package com.comcast.crm.objectRepository_utility;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class CreateDocumentPage {
